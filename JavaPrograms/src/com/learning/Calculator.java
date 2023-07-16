f588428e280c2e512c9f65dbe3ff082e4d2bb2cf
sachinsomanwyd <sachinsomanwyd@gmail.com>
2023-07-16 12:38:09 +0530

    added subtract method

--- JavaPrograms/src/com/learning/Calculator.java
+++ JavaPrograms/src/com/learning/Calculator.java
@@ -14,11 +14,16 @@ public class Calculator {
 		
 		Calculator c = new Calculator();
 		System.out.println("Addition : "+ c.add(a, b));
-
+		
+		System.out.println("Subtraction : "+ c.subtraction(a, b));
 	}
 	
 	public int add(int a, int b) {
 		return a+b;
+	}
+	
+	public int subtraction(int a, int b) {
+		return a-b;
 	}
 	
 
