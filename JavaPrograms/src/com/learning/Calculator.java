9af95c28a9efbdd14554b3c8bbc7ac8a64d42b3c
sachinsomanwyd <sachinsomanwyd@gmail.com>
2023-07-16 12:34:07 +0530

    added calculator.java file

---
+++ JavaPrograms/src/com/learning/Calculator.java
@@ -0,0 +1,25 @@
+package com.learning;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int a=0,b=0;
+		System.out.println("Enter two numbers");
+		
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		Calculator c = new Calculator();
+		System.out.println("Addition : "+ c.add(a, b));
+
+	}
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+
+}
