14f910f5a66ed41e76b11275116c5ead7e0bb0bc
sachinsomanwyd <sachinsomanwyd@gmail.com>
2023-07-17 10:32:59 +0530

    Switch statement

---
+++ JavaPrograms/src/com/learning/DayName.java
@@ -0,0 +1,39 @@
+package com.learning;
+
+import java.util.Scanner;
+
+public class DayName {
+
+	public static void main(String[] args) {
+		int d;
+		System.out.println("enter the number");
+		Scanner sc = new Scanner(System.in);
+		d = sc.nextInt();
+		switch (d) {
+		case 1:
+			System.out.println("Sunday");
+			break;
+		case 2:
+			System.out.println("Monday");
+			break;
+		case 3:
+			System.out.println("Tuesday");
+			break;
+		case 4:
+			System.out.println("Wednesday");
+			break;
+		case 5:
+			System.out.println("Thursday");
+			break;
+		case 6:
+			System.out.println("Friday");
+			break;
+		case 7:
+			System.out.println("Saturday");
+			break;
+		default:
+			System.out.println("wrong input");
+			break;
+		}
+	}
+}
