88e38d22a9a92c958006cf3e012acc18a885382d
sachinsomanwyd <sachinsomanwyd@gmail.com>
2023-07-19 19:50:33 +0530

    Square root of a Number

---
+++ JavaPrograms/src/com/learning/SquareRoot.java
@@ -0,0 +1,16 @@
+package com.learning;
+
+import java.util.Scanner;
+
+public class SquareRoot {
+
+	public static void main(String[] args) {
+		int n;
+		System.out.println("Enter the number");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+        System.out.println("The square root of "+n+" is "+Math.sqrt(n));
+        
+	}
+
+}
