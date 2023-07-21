ae325dbe93c25452e372f0f459ffe8a379115772
sachinsomanwyd <sachinsomanwyd@gmail.com>
2023-07-21 20:12:31 +0530

    Length of a string

---
+++ JavaPrograms/src/com/learning/StringLength.java
@@ -0,0 +1,16 @@
+package com.learning;
+
+import java.util.Scanner;
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		String word;
+		System.out.println("Enter the word");
+		Scanner sc = new Scanner(System.in);
+		word = sc.nextLine();
+		System.out.println(word.length());
+
+	}
+
+}
