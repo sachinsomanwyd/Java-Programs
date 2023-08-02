32a097f7941548a03ec622c2d61641fa255056cf
sachinsomanwyd <sachinsomanwyd@gmail.com>
2023-08-02 20:41:06 +0530

    Harshad Number

---
+++ JavaPrograms/src/com/learning/HarshadNumber.java
@@ -0,0 +1,26 @@
+package com.learning;
+
+import java.util.Scanner;
+
+public class HarshadNumber {
+
+	public static void main(String[] args) {
+		int n, sum = 0, m, d;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number");
+		n = in.nextInt();
+		m = n;
+		while (n != 0) {
+			d = n % 10;
+			sum = sum + d;
+			n = n / 10;
+		}
+		if (m % sum == 0) {
+			System.out.println("Harshad number");
+		} else {
+			System.out.println("Not a harshad number");
+		}
+
+	}
+
+}
