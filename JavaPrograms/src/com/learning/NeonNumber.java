0364d4e4175ca7d93beb81fd39023c9379f0bbff
sachinsomanwyd <sachinsomanwyd@gmail.com>
2023-08-02 20:53:01 +0530

    Neon Number

---
+++ JavaPrograms/src/com/learning/NeonNumber.java
@@ -0,0 +1,27 @@
+package com.learning;
+
+import java.util.Scanner;
+
+public class NeonNumber {
+
+	public static void main(String[] args) {
+		int n, sum = 0, m, d;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number");
+		n = in.nextInt();
+		m = n;
+		n = n * n;
+		while (n != 0) {
+			d = n % 10;
+			sum = sum + d;
+			n = n / 10;
+		}
+		if (sum == m) {
+			System.out.println("Neon number");
+		} else {
+			System.out.println("Not a neon number");
+		}
+
+	}
+
+}
